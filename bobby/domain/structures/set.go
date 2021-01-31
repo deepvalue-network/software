d408cf72466d847c0f222ad10355e0468fc0c9c0
Steve Rodrigue <stev.rodr@gmail.com>
2021-01-31 04:45:04 -0500

    Added the set struct

---
+++ bobby/domain/structures/set.go
@@ -0,0 +1,43 @@
+package structures
+
+import (
+	"github.com/steve-care-software/products/bobby/domain/structures/sets"
+	set_schemas "github.com/steve-care-software/products/bobby/domain/structures/sets/schemas"
+)
+
+type set struct {
+	schema set_schemas.Schema
+	set    sets.Set
+}
+
+func createSetInternally(
+	schema set_schemas.Schema,
+	st sets.Set,
+) Set {
+	out := set{
+		schema: schema,
+		set:    st,
+	}
+
+	return &out
+}
+
+// IsSchema returns true if there is a schema, false otherwise
+func (obj *set) IsSchema() bool {
+	return obj.schema != nil
+}
+
+// Schema returns the schema, if any
+func (obj *set) Schema() set_schemas.Schema {
+	return obj.schema
+}
+
+// IsSet returns true if there is a set, false otherwise
+func (obj *set) IsSet() bool {
+	return obj.set != nil
+}
+
+// Set returns the set, if any
+func (obj *set) Set() sets.Set {
+	return obj.set
+}
