f45e0fb3614d774144dd1b7521063528c1914c24
Steve Rodrigue <stev.rodr@gmail.com>
2021-02-19 05:26:49 -0500

    Separated the identity from the government application

--- governments/application/sdk.go
+++ governments/application/sdk.go
@@ -6,17 +6,22 @@ import (
 	"github.com/deepvalue-network/software/governments/domain/governments/shareholders"
 	"github.com/deepvalue-network/software/governments/domain/propositions"
 	"github.com/deepvalue-network/software/governments/domain/resolutions"
-	"github.com/deepvalue-network/software/libs/cryptography/pk/signature"
 	"github.com/deepvalue-network/software/libs/hash"
 	uuid "github.com/satori/go.uuid"
 )
 
 // Application represents the government application
 type Application interface {
+	Identity() Identity
+	Government() Government
+}
+
+// Government represents the government application
+type Government interface {
+	List() ([]*uuid.UUID, error)
 	Retrieve(id *uuid.UUID) (governments.Government, error)
-	Proposition(governmentID *uuid.UUID) Proposition
-	Resolution(governmentID *uuid.UUID) Resolution
-	ShareHolder(governmentID *uuid.UUID) ShareHolder
+	Proposition(id *uuid.UUID) Proposition
+	Resolution(id *uuid.UUID) Resolution
 }
 
 // Proposition represents the proposition application
@@ -34,9 +39,9 @@ type Resolution interface {
 	Retrieve(hash hash.Hash) (resolutions.Resolution, error)
 }
 
-// ShareHolder represents the shareholder application
-type ShareHolder interface {
+// Identity represents the identity application
+type Identity interface {
 	List() ([]hash.Hash, error)
 	Retrieve(hash hash.Hash) (shareholders.ShareHolder, error)
-	Authenticate(hash hash.Hash, pk signature.PrivateKey) authenticated.Application
+	Authenticate(name string, seed string, password string) authenticated.Application
 }
