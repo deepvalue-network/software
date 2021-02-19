64070ec0286b8ea1a4ba104cc3d3087475f55207
Steve Rodrigue <stev.rodr@gmail.com>
2021-02-19 04:29:48 -0500

    Added the outgoings

--- governments/domain/shareholders/transactions/transfers/outgoings/sdk.go
+++ governments/domain/shareholders/transactions/transfers/outgoings/sdk.go
@@ -7,12 +7,25 @@ import (
 	"github.com/deepvalue-network/software/libs/hash"
 )
 
-// Builder represents an outgoing builder
+// Builder represents an outgoings builder
 type Builder interface {
 	Create() Builder
-	WithTransfer(transfer views.Section) Builder
-	WithNote(note string) Builder
-	CreatedOn(createdOn time.Time) Builder
+	WithOutgoings(outgoings []Outgoing) Builder
+	Now() (Outgoings, error)
+}
+
+// Outgoings represents an outgoings
+type Outgoings interface {
+	Hash() hash.Hash
+	All() []Outgoing
+}
+
+// OutgoingBuilder represents an outgoing builder
+type OutgoingBuilder interface {
+	Create() OutgoingBuilder
+	WithTransfer(transfer views.Section) OutgoingBuilder
+	WithNote(note string) OutgoingBuilder
+	CreatedOn(createdOn time.Time) OutgoingBuilder
 	Now() (Outgoing, error)
 }
 
