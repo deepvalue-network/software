5cd6d5ce7cba7a628ca5b55a5c0b3983c9d50b1a
Steve Rodrigue <stev.rodr@gmail.com>
2021-02-19 04:30:38 -0500

    Fixed the transactuions

--- governments/domain/shareholders/transactions/sdk.go
+++ governments/domain/shareholders/transactions/sdk.go
@@ -2,19 +2,19 @@ package transactions
 
 import (
 	"github.com/deepvalue-network/software/governments/domain/shareholders/transactions/payments"
-    "github.com/deepvalue-network/software/governments/domain/shareholders/transactions/transfers"
+	"github.com/deepvalue-network/software/governments/domain/shareholders/transactions/transfers"
 )
 
 // Builder represents a transactions builder
 type Builder interface {
-    Create() Builder
-    WithPayments(payments []payments.Payment) Builder
-    WithTransfers(transfers transfers.Transfers) Builder
-    Now() (Transactions, error)
+	Create() Builder
+	WithPayments(payments payments.Payments) Builder
+	WithTransfers(transfers transfers.Transfers) Builder
+	Now() (Transactions, error)
 }
 
 // Transactions represents the transactions
 type Transactions interface {
-	Payments() []payments.Payment
+	Payments() payments.Payments
 	Transfers() transfers.Transfers
 }
