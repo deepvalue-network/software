3732db765a7585747779000b79546d6a9cea2b8c
Steve Rodrigue <stev.rodr@gmail.com>
2021-04-21 11:32:41 -0400

    Added the language application label instructions structs

--- pangolin/domain/middle/languages/applications/labels/label/instructions/sdk.go
+++ pangolin/domain/middle/languages/applications/labels/label/instructions/sdk.go
@@ -2,7 +2,19 @@ package instructions
 
 import "github.com/deepvalue-network/software/pangolin/domain/middle/languages/applications/labels/label/instructions/instruction"
 
+// NewBuilder creates a new builder instance
+func NewBuilder() Builder {
+	return createBuilder()
+}
+
+// Builder represents an instructions builder
+type Builder interface {
+	Create() Builder
+	WithList(list []instruction.Instruction) Builder
+	Now() (Instructions, error)
+}
+
 // Instructions represents instructions
 type Instructions interface {
-	Instructions() []instruction.Instruction
+	All() []instruction.Instruction
 }
