018c436f67061809314bf0804813cdb091440041
Steve Rodrigue <stev.rodr@gmail.com>
2021-04-21 11:13:24 -0400

    Added the language application instructions structs

--- pangolin/domain/middle/languages/applications/instructions/sdk.go
+++ pangolin/domain/middle/languages/applications/instructions/sdk.go
@@ -4,6 +4,18 @@ import (
 	"github.com/deepvalue-network/software/pangolin/domain/middle/languages/applications/instructions/instruction"
 )
 
+// NewBuilder creates a new builder instance
+func NewBuilder() Builder {
+	return createBuilder()
+}
+
+// Builder represents an instructions builder
+type Builder interface {
+	Create() Builder
+	WithList(list []instruction.Instruction) Builder
+	Now() (Instructions, error)
+}
+
 // Instructions represents a language instructions
 type Instructions interface {
 	All() []instruction.Instruction
