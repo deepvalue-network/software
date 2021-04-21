842702b76e03b77ff7af8c89d9a2fe997d64d5a7
Steve Rodrigue <stev.rodr@gmail.com>
2021-04-21 11:42:52 -0400

    Added the language application label  structs

--- pangolin/domain/middle/languages/applications/labels/label/sdk.go
+++ pangolin/domain/middle/languages/applications/labels/label/sdk.go
@@ -2,6 +2,19 @@ package label
 
 import "github.com/deepvalue-network/software/pangolin/domain/middle/languages/applications/labels/label/instructions"
 
+// NewBuilder creates a new builder instance
+func NewBuilder() Builder {
+	return createBuilder()
+}
+
+// Builder represents a label builder
+type Builder interface {
+	Create() Builder
+	WithName(name string) Builder
+	WithInstructions(instructions instructions.Instructions) Builder
+	Now() (Label, error)
+}
+
 // Label represents a label
 type Label interface {
 	Name() string
