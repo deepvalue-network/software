7acbc1a071761d5c4a7d575970cc1fe6a94930c4
Steve Rodrigue <stev.rodr@gmail.com>
2021-04-21 14:49:13 -0400

    Added the test structs

--- pangolin/domain/middle/languages/applications/tests/test/sdk.go
+++ pangolin/domain/middle/languages/applications/tests/test/sdk.go
@@ -4,6 +4,19 @@ import (
 	"github.com/deepvalue-network/software/pangolin/domain/middle/languages/applications/tests/test/instructions"
 )
 
+// NewBuilder creates a new builder instance
+func NewBuilder() Builder {
+	return createBuilder()
+}
+
+// Builder represents a test builder
+type Builder interface {
+	Create() Builder
+	WithName(name string) Builder
+	WithInstructions(ins instructions.Instructions) Builder
+	Now() (Test, error)
+}
+
 // Test represents a test
 type Test interface {
 	Name() string
