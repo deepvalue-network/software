218896027bb039d4327f8a4b90f97ed59e1cbc12
Steve Rodrigue <stev.rodr@gmail.com>
2021-04-21 11:31:50 -0400

    Added the language application label instruction structs

---
+++ pangolin/domain/middle/languages/applications/labels/label/instructions/instruction/instruction.go
@@ -0,0 +1,75 @@
+package instruction
+
+import (
+	label_instruction "github.com/deepvalue-network/software/pangolin/domain/middle/labels/label/instructions/instruction"
+	language_instruction "github.com/deepvalue-network/software/pangolin/domain/middle/languages/applications/instructions/instruction"
+	"github.com/deepvalue-network/software/pangolin/domain/middle/languages/applications/labels/label/instructions/instruction/token"
+)
+
+type instruction struct {
+	label label_instruction.Instruction
+	lang  language_instruction.Instruction
+	tok   token.Token
+}
+
+func createInstructionWithLabel(
+	label label_instruction.Instruction,
+) Instruction {
+	return createInstructionInternally(label, nil, nil)
+}
+
+func createInstructionWithLanguage(
+	lang language_instruction.Instruction,
+) Instruction {
+	return createInstructionInternally(nil, lang, nil)
+}
+
+func createInstructionWithToken(
+	tok token.Token,
+) Instruction {
+	return createInstructionInternally(nil, nil, tok)
+}
+
+func createInstructionInternally(
+	label label_instruction.Instruction,
+	lang language_instruction.Instruction,
+	tok token.Token,
+) Instruction {
+	out := instruction{
+		label: label,
+		lang:  lang,
+		tok:   tok,
+	}
+
+	return &out
+}
+
+// IsLabel returns true if there is a label, false otherwise
+func (obj *instruction) IsLabel() bool {
+	return obj.label != nil
+}
+
+// Label returns the label, if any
+func (obj *instruction) Label() label_instruction.Instruction {
+	return obj.label
+}
+
+// IsLanguage returns true if there is a language, false otherwise
+func (obj *instruction) IsLanguage() bool {
+	return obj.lang != nil
+}
+
+// Language returns the language, if any
+func (obj *instruction) Language() language_instruction.Instruction {
+	return obj.lang
+}
+
+// IsToken returns true if there is a token, false otherwise
+func (obj *instruction) IsToken() bool {
+	return obj.tok != nil
+}
+
+// Token returns the token, if any
+func (obj *instruction) Token() token.Token {
+	return obj.tok
+}
--- pangolin/domain/middle/languages/applications/labels/label/instructions/instruction/sdk.go
+++ pangolin/domain/middle/languages/applications/labels/label/instructions/instruction/sdk.go
@@ -6,6 +6,20 @@ import (
 	"github.com/deepvalue-network/software/pangolin/domain/middle/languages/applications/labels/label/instructions/instruction/token"
 )
 
+// NewBuilder creates a new builder instance
+func NewBuilder() Builder {
+	return createBuilder()
+}
+
+// Builder represents an instruction builder
+type Builder interface {
+	Create() Builder
+	WithLabel(label label_instruction.Instruction) Builder
+	WithLanguage(lang language_instruction.Instruction) Builder
+	WithToken(token token.Token) Builder
+	Now() (Instruction, error)
+}
+
 // Instruction represents a language label instruction
 type Instruction interface {
 	IsLabel() bool
